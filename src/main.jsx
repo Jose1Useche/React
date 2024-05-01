34182257b0d578208c4d22e9409810352b254803
Jose Useche <jose1useche@gmail.com>
2024-05-01 17:14:10 -0400

    Tarea al pelo de cochino

--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,7 @@ import "./styles.css";
 import { HelloWorldApp } from "./HelloWorldApp";
 import { FirstApp } from "./FirstApp";
 import { PropsApp } from "./PropsApp";
+import { CounterApp } from "./CounterApp";
 
 
 //El "root" es el elemento que se encuentra en index.html
@@ -13,5 +14,6 @@ ReactDOM.createRoot(document.getElementById('root')).render(
         <HelloWorldApp/>
         <FirstApp/>
         <PropsApp title="Hola Souy Goku" subtitle={123} subtitle2="123" requiredTitle="{123}"/>
+        <CounterApp value={123}/>
     </React.StrictMode>
 )
