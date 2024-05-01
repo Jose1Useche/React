34a953d67d4380d5507e5fc2b942aed7bef1ac8e
Jose Useche <jose1useche@gmail.com>
2024-05-01 14:00:28 -0400

    Separando nuestro primer componente del main (HelloWorldApp.jsx)

---
+++ src/HelloWorldApp.jsx
@@ -0,0 +1,4 @@
+export function HelloWorldApp() {
+    // document.createElement...
+    return <h1>Hola Mundo</h1>
+}
--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,12 @@
 import React from "react";
 import ReactDOM from 'react-dom/client';
 
-function App() {
-    // document.createElement...
-    return <h1>Hola Mundo</h1>
-}
+import { HelloWorldApp } from "./HelloWorldApp";
+
 
 //El "root" es el elemento que se encuentra en index.html
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
-        <App></App>
+        <HelloWorldApp></HelloWorldApp>
     </React.StrictMode>
 )
